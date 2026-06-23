bd7c487132388e0466007314332580447d3ef3f8
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-06-22 22:08:11 -0300

    feat(saloon): add repository with search queries

---
+++ src/main/java/com/cauanlagrotta/repository/SaloonRepository.java
@@ -0,0 +1,21 @@
+package com.cauanlagrotta.repository;
+
+import com.cauanlagrotta.model.Saloon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SaloonRepository extends JpaRepository<Saloon, Long> {
+
+  Saloon findByOwnerId(Long ownerId);
+
+  @Query("select s from Saloon s where" +
+      "(lower(s.city) like lower(concat('%', :keyword, '%')) OR + " +
+      "lower(s.name) like lower(concat('%', :keyword, '%')) OR + " +
+      "lower(s.address) like lower(concat('%', :keyword, '%')))"
+  )
+  List<Saloon> searchSaloons(@Param("keyword") String keyword);
+
+}
