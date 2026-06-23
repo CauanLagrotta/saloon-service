abd6cda07dc55a5e2fff66fe6b94be6ced7c8f57
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-06-22 22:08:18 -0300

    refactor(saloon): add Lombok annotations to SaloonDTO

--- src/main/java/com/cauanlagrotta/payload/dto/SaloonDTO.java
+++ src/main/java/com/cauanlagrotta/payload/dto/SaloonDTO.java
@@ -1,10 +1,14 @@
 package com.cauanlagrotta.payload.dto;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalTime;
 import java.util.List;
 
+@Getter
+@Setter
 public class SaloonDTO {
 
   private Long id;
